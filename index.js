2a3ba3a181db44db038cc94a942144781363dccd
Mahin Hasan <mahinmtrs@gmail.com>
2024-01-21 06:33:11 +0600

    added jwt token and secured user api for admin only

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -37,12 +38,70 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+    //JWT related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+    })
+    //middle ware for token verification
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      //must use same name set to client side ig authorization
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];// first text is bearer set from client we want token so index 1
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();// if jwt verified then it will execute next
+      })
+
+
+      // if(!token){
+      //can do some operations
+      // }
+      // next();
+    }
+    //doing admin verification like a middle ware || notice as it is coming after decoded so lets assume we have token
+    //also check user role
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+/*
+Notes:
+verifyToken, verifyAdmin secures the api not the client side routes so routes should be implemented in client side
+*/
     //user related api for handling role used in signup
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => { //two step verification || users must have token also need to be admin
+      console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     })
 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {//decoded is set in jwt verify token and email is set in client side auth provider unSubscribe || my token my email check
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
     app.post('/users', async (req, res) => {
       const user = req.body;
       //insert email if user does not exist
@@ -56,7 +115,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -68,7 +127,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
